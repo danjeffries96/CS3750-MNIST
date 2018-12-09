6e1b630ca818cea540baefee16c94ba27095c7d5
Damn Peggy <ericrusso97@gmail.com>
2018-12-09 11:38:34 -0500

    test123

--- cnn.py
+++ cnn.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.contrib.layers import flatten
+from sklearn.utils import shuffle
 import numpy as np
 
 X = np.load("x_mnist1000.npy")
@@ -16,64 +16,73 @@ test_ind = indices[900:]
 X_train = X[train_indices]
 y_train = y[train_indices]
 
+X_validation = X[valid_ind]
+y_validation = y[valid_ind]
+
+X_test = X[test_ind]
+y_test = y[test_ind]
+
 # pad for lenet
 pad_dims = ((0, 0), (2, 2), (2, 2), (0, 0))
 X_train = np.pad(X_train, pad_dims, "constant")
+X_validation = np.pad(X_validation, pad_dims, "constant")
+X_test = np.pad(X_test, pad_dims, "constant")
 
-# Conv2d
-# input: A Tensor. Must be one of the following types: half, bfloat16, float32, float64. A 4-D tensor. The dimension order is interpreted according to the value of data_format, see below for details.
-# filter: A Tensor. Must have the same type as input. 
-#         A 4-D tensor of shape 
-#         [filter_height, filter_width, in_channels, out_channels]
-# strides: A list of ints. 1-D tensor of length 4. The stride of the sliding window for each dimension of input. The dimension order is determined by the value of data_format, see below for details.
-# padding: A string from: "SAME", "VALID". The type of padding algorithm to use.
 
 X_inp = tf.placeholder(tf.float32, shape=(None, 32, 32, 1))
-conv1_fmaps = np.zeros(shape=(5, 5, 1, 6))
-conv1 = tf.nn.conv2d(X_inp, conv1_fmaps, strides=[1, 1, 1, 1], padding="VALID")
-conv1 = tf.nn.tanh(conv1)
+y_inp = tf.placeholder(tf.int32, (None))
 
-avg_pool = tf.nn.avg_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
-                          padding="VALID")
+conv1 = tf.layers.conv2d(X_inp, filters=6, kernel_size=5,  strides=1, padding="valid", activation=tf.nn.tanh) 
+
+avg_pool = tf.layers.average_pooling2d(conv1, pool_size=2, strides=2)
 avg_pool = tf.nn.tanh(avg_pool)
 
-conv2_fmaps = np.zeros(shape=(10, 10, 6, 16))
-conv2 = tf.nn.conv2d(avg_pool, conv2_fmaps, strides=[1, 1, 1, 1],
-                       padding="VALID")
-conv2 = tf.nn.tanh(conv2)
+conv2 = tf.layers.conv2d(avg_pool, filters=16, kernel_size = 5, strides=1,
+                       padding="valid", activation = tf.nn.tanh)
 
-avg_pool2 = tf.nn.avg_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 1, 1, 1],
-                            padding="VALID")
+avg_pool2 = tf.layers.average_pooling2d(conv2, pool_size = 2, strides=2)
 avg_pool2 = tf.nn.tanh(avg_pool2)
 
-conv3_fmaps = np.zeros(shape=(1, 1, 16, 120))
-conv3 = tf.nn.conv2d(avg_pool2, conv3_fmaps, strides=[1, 1, 1, 1],
-                      padding="VALID")
-conv3 = tf.nn.tanh(conv3)
+conv3 = tf.layers.conv2d(avg_pool2, filters=120, kernel_size = 5, strides=1,
+                      padding="valid", activation = tf.nn.tanh)
 
 # flatten for dense layers
-fl = flatten(conv3)
+flat = tf.reshape(conv3, [-1, 120])
 
-dense = tf.layers.dense(inputs=fl, units=84, activation=tf.nn.tanh)
+dense = tf.layers.dense(inputs=flat, units=84, activation=tf.nn.tanh)
 
 logits = tf.layers.dense(dense, units=10)
 
 softmax = tf.nn.softmax(logits)
-final = tf.argmax(softmax, axis=1)
-  
-y_labels = tf.one_hot(y_train, 10)
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_labels)
+predict = tf.argmax(softmax, axis=1)
+
+y_labels = tf.one_hot(y_inp, 10)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=y_labels)
 loss = tf.reduce_mean(cross_entropy)
-opt = tf.train.AdamOptimizer(learning_rate=0.1)
+opt = tf.train.AdamOptimizer(learning_rate=0.001)
 train_op = opt.minimize(loss)
+
+epochs = 10
+N = len(X_train)
+BATCH_SIZE = N // 10 
+
+def evaluate(X_eval, y_eval):
+    sess = tf.get_default_session()
+    out = sess.run(predict, feed_dict ={X_inp: X_eval, y_inp: y_eval})
+    return np.mean(out == y_eval)
 
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
-  sess.run(train_op, feed_dict={X_inp: X_train})
 
-  output = sess.run(final, feed_dict={X_inp: X_train})
-  print(output)
+  for i in range(epochs):
+      X_train, y_train = shuffle(X_train, y_train)
+      for off in range(0, N, BATCH_SIZE):
+          end = off + BATCH_SIZE
+          batch_x, batch_y = X_train[off:end], y_train[off:end]
+          sess.run(train_op, feed_dict={X_inp: batch_x, y_inp : batch_y})
+      valid_acc = evaluate(X_validation, y_validation) 
+      print("Validation accuracy: " + str(valid_acc))
+  test_acc = evaluate(X_test, y_test) 
+  print("Test accuracy: " + str(test_acc))
 
-  print(np.mean(output == y_train))
-   
 
