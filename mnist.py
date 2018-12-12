ecca8aaaff5d4d77534af0f2bba72cb572228141
danjeffries96 <danjeffries96@github.com>
2018-12-12 12:48:36 -0500

    done testing dnn

--- mnist.py
+++ mnist.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+import pandas as pd
+from datetime import datetime
 
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.exceptions import NotFittedError
@@ -12,8 +14,9 @@ he_init = tf.variance_scaling_initializer()
 
 class DNNClassifier(BaseEstimator, ClassifierMixin):
     def __init__(self, n_hidden_layers=5, n_neurons=100, optimizer_class=tf.train.AdamOptimizer,
-                 learning_rate=0.01, batch_size=20, activation=tf.nn.elu, initializer=he_init,
-                 batch_norm_momentum=None, dropout_rate=None, random_state=None, checks=20):
+                 learning_rate=0.001, batch_size=20, activation=tf.nn.elu, initializer=he_init,
+                 batch_norm_momentum=None, dropout_rate=None, random_state=None, checks=20,
+                 X_valid=None, y_valid=None, max_iter=20000):
         """Initialize the DNNClassifier by simply storing all the hyperparameters."""
         self.n_hidden_layers = n_hidden_layers
         self.n_neurons = n_neurons
@@ -27,6 +30,10 @@ class DNNClassifier(BaseEstimator, ClassifierMixin):
         self.random_state = random_state
         self._session = None
         self.checks = checks
+
+        self.X_valid = X_valid
+        self.y_valid = y_valid
+        self.max_iter = max_iter
 
     def _dnn(self, inputs):
         """Build the hidden layers, with support for batch normalization and dropout."""
@@ -103,6 +110,9 @@ class DNNClassifier(BaseEstimator, ClassifierMixin):
         """Fit the model to the training set. If X_valid and y_valid are provided, use early stopping."""
         self.close_session()
 
+        X_valid = X_valid or self.X_valid
+        y_valid = y_valid or self.y_valid
+
         # infer n_inputs and n_outputs from the training set.
         n_inputs = X.shape[1]
         self.classes_ = np.unique(y)
@@ -129,14 +139,27 @@ class DNNClassifier(BaseEstimator, ClassifierMixin):
         checks_without_progress = 0
         best_loss = np.infty
         best_params = None
-        
+
+        # run for fixed number of iterations
+        num_runs = 0
+        self.val_acc = []
+
         # Now train the model!
         self._session = tf.Session(graph=self._graph)
         with self._session.as_default() as sess:
             self._init.run()
-            for epoch in range(n_epochs):
+            # for epoch in range(n_epochs):
+            while True:
+                epoch = 0
                 rnd_idx = np.random.permutation(len(X))
                 for rnd_indices in np.array_split(rnd_idx, len(X) // self.batch_size):
+
+                    num_runs += self.batch_size
+                    if num_runs > self.max_iter:
+                        print("Early stopping!", self.max_iter, "samples trained on.")
+                        self.save_val_plot()
+                        return
+
                     X_batch, y_batch = X[rnd_indices], y[rnd_indices]
                     feed_dict = {self._X: X_batch, self._y: y_batch}
                     if self._training is not None:
@@ -148,6 +171,7 @@ class DNNClassifier(BaseEstimator, ClassifierMixin):
                     loss_val, acc_val = sess.run([self._loss, self._accuracy],
                                                  feed_dict={self._X: X_valid,
                                                             self._y: y_valid})
+                    self.val_acc.append({"Epoch": num_runs, "Validation": acc_val})
                     if loss_val < best_loss:
                         best_params = self._get_model_params()
                         best_loss = loss_val
@@ -156,9 +180,9 @@ class DNNClassifier(BaseEstimator, ClassifierMixin):
                         checks_without_progress += 1
                     print("{}\tValidation loss: {:.6f}\tBest loss: {:.6f}\tAccuracy: {:.2f}%".format(
                         epoch, loss_val, best_loss, acc_val * 100))
-                    if checks_without_progress > max_checks_without_progress:
-                        print("Early stopping!")
-                        break
+                    # if checks_without_progress > max_checks_without_progress:
+                    #     print("Early stopping!")
+                    #     break
                 else:
                     loss_train, acc_train = sess.run([self._loss, self._accuracy],
                                                      feed_dict={self._X: X_batch,
@@ -168,7 +192,15 @@ class DNNClassifier(BaseEstimator, ClassifierMixin):
             # If we used early stopping then rollback to the best model found
             if best_params:
                 self._restore_model_params(best_params)
+
+            self.save_val_plot()
             return self
+
+    def save_val_plot(self):
+        df = pd.DataFrame(self.val_acc, columns=["Epoch", "Validation"])
+        now = datetime.now().strftime("%H-%M-%S")
+        fn = "./val_scores/" + now + ".csv"
+        df.to_csv(fn, index=False)
 
     def predict_proba(self, X):
         if not self._session:
@@ -200,11 +232,15 @@ def main():
   # split data
   indices = np.random.permutation(M)
   train_indices = indices[:800]
-  test_indices = indices[800:]
+  val_indices = indices[800:900]
+  test_indices = indices[900:]
   
   X_train = X[train_indices]
   Y_train = Y[train_indices]
   y_train = y[train_indices]
+
+  X_valid = X[val_indices]
+  y_valid = y[val_indices]
   
   X_test = X[test_indices]
   Y_test = Y[test_indices]
@@ -220,48 +256,50 @@ def main():
   # scale train and test data
   X_train = (X_train - X_train_mean) / X_train_std
   X_test = (X_test - X_train_mean) / X_train_std
+  X_valid = (X_valid - X_train_mean) / X_train_std
 
   def leaky_relu(alpha=0.01):
     def parametrized_leaky_relu(z, name=None):
       return tf.maximum(alpha * z, z, name=name)
     return parametrized_leaky_relu
 
-  dnn_clf = \
-    DNNClassifier(random_state=1 , n_hidden_layers=1, activation=tf.nn.elu, batch_size=100,
-                  dropout_rate=0.1)
-  # dnn_clf.fit(X_train, y_train, n_epochs=10002
+  dnn_clf = DNNClassifier(random_state=1)
 
   param_distribs = {
-    "n_neurons": [10, 30, 50, 70, 90, 100, 120, 140, 160],
+    # "n_neurons": [10, 30, 50, 70, 90, 100, 120, 140, 160],
     # "batch_size": [10, 50, 100, 500],
     # "learning_rate": [0.01, 0.02, 0.05, 0.1],
-    # "activation": [tf.nn.relu, tf.nn.elu, leaky_relu(alpha=0.01), leaky_relu(alpha=0.1)],
+    # "activation": [tf.nn.relu, tf.nn.tanh, tf.nn.elu, tf.nn.selu],
+    "activation": [tf.nn.selu],
     # you could also try exploring different numbers of hidden layers, different optimizers, etc.
-    #"n_hidden_layers": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "n_hidden_layers": [1, 2, 3],
     # "optimizer_class": [tf.train.AdamOptimizer, partial(tf.train.MomentumOptimizer, momentum=0.95)],
-    "optimizer_class": [partial(tf.train.MomentumOptimizer, momentum=0.95)],
+    # "dropout_rate": [0, 0.1, 0.2],
+    "X_valid": [X_valid],
+    "y_valid": [y_valid],
+    "max_iter": [40000],
   }
-   
 
-  # rnd_search = RandomizedSearchCV(DNNClassifier(random_state=42), param_distribs, n_iter=1,
-  #                                                 random_state=42, verbose=3)
-  # rnd_search.fit(X_train, y_train)
+  gs = GridSearchCV(dnn_clf, param_distribs, n_jobs=-1)
+  gs.fit(X_train, y_train)
 
-  # best = rnd_search.best_params_
-  # bs = rnd_search.best_score_
-  # print(bs, best)
-  # dnn_clf = rnd_search.best_estimator_
-  dnn_clf.fit(X_train, y_train)
+  clf = gs.best_estimator_
+  print("Best score:", gs.best_score_)
+  print("Params:", gs.best_params_)
 
-  y_pred_train = dnn_clf.predict(X_train)
-  cv_scores = cross_val_score(dnn_clf, X_train, y_train, cv=5)
-  y_pred = dnn_clf.predict(X_test)
+  y_pred_train = clf.predict(X_train)
+  cv_scores = cross_val_score(clf, X_train, y_train, cv=5)
+  y_pred = clf.predict(X_test)
 
   print("Training accuracy:", accuracy_score(y_train, y_pred_train))
   print("Cross val scores:", cv_scores)
   print("Mean:", np.mean(cv_scores))
   print("std:", np.std(cv_scores))
   print("Test accuracy:", accuracy_score(y_test, y_pred))
+
+  df = pd.DataFrame(gs.cv_results_)
+  now = datetime.now().strftime("%H-%M-%S")
+  df.to_csv("./gs_results/" + now + ".csv", index=False)
 
 main()
  
